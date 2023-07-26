1b75608c1ecb013cb61a7ca6d75474c5a1b39879
VKTRenokh <viktorenokh@gmail.com>
2023-07-26 09:37:03 +0300

    feat: add database service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DataBaseService } from './database/database.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly dataBaseSerice: DataBaseService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getUsers() {
+    return this.dataBaseSerice.getUsers();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataBaseService } from './database/database.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DataBaseService],
 })
 export class AppModule {}
