e4bb34b561aa76abe3c2983cab3ff31ba17cb0f6
VKTRenokh <viktorenokh@gmail.com>
2023-07-26 16:01:53 +0300

    feat: add find user by id

--- src/query/database/database.service.ts
+++ src/query/database/database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
 import { Db } from 'src/types/db';
+import { User } from 'src/types/user';
 
 @Injectable()
 export class DatabaseService {
@@ -20,15 +21,29 @@ export class DatabaseService {
     ],
   };
 
+  parseUser(user: User) {
+    return {
+      login: user.login,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      version: user.version,
+      id: user.id,
+    };
+  }
+
   getAllUsers() {
     return this.db.users.map((user) => {
-      return {
-        login: user.login,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-        version: user.version,
-        id: user.id,
-      };
+      return this.parseUser(user);
     });
+  }
+
+  getUserById(id: string) {
+    const user = this.db.users.find((user) => user.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return this.parseUser(user);
   }
 }
--- src/query/users/users.controller.ts
+++ src/query/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, Param } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
 
 @Controller('user')
@@ -6,7 +6,18 @@ export class UsersController {
   constructor(private readonly database: DatabaseService) {}
 
   @Get()
-  getAllUsers() {
+  getAll() {
     return this.database.getAllUsers();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    const user = this.database.getUserById(id);
+
+    if (!user) {
+      throw new HttpException('not found', 404);
+    }
+
+    return user;
   }
 }
