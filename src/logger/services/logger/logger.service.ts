485794d710ec6a74ce4b32a35ea8ebfa1df4d417
VKTRenokh <viktorenokh@gmail.com>
2023-08-21 16:56:45 +0300

    feat: add useLogger

--- src/logger/services/logger/logger.service.ts
+++ src/logger/services/logger/logger.service.ts
@@ -4,11 +4,21 @@ import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
   scope: Scope.TRANSIENT
 })
 export class LoggerService extends ConsoleLogger {
-  isLogLevelEnabled(logLevel: number) {
-    console.log(process.env.LOG_LEVEL)
+  log(message: any, context?: string): void;
+  log(message: any, ...optionalParams: any[]): void;
+  log(message: unknown, context?: unknown): void {
+    super.log(message, context)
   }
 
-  log(message: unknown, ...messages: unknown[]) {
-    super.log(message)
+  warn(message: any, context?: string): void;
+  warn(message: any, ...optionalParams: any[]): void;
+  warn(message: unknown, context?: unknown): void {
+    super.warn(message, context)
+  }
+
+  error(message: any, stack?: string, context?: string): void;
+  error(message: any, ...optionalParams: any[]): void;
+  error(message: unknown, stack?: unknown, context?: unknown): void {
+    super.error(message, stack, context)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,26 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { components, paths, security } from 'doc/doc';
 import { AppModule } from './app.module';
+import { LoggerService } from './logger/services/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = new ConfigService().get('PORT') || 4000;
   app.enableCors();
+
+  const logger = await app.resolve(LoggerService)
+
+  app.useLogger(logger)
+
+  process.on('uncaughtException', (reason) => {
+    logger.log('uncaught exception')
+    logger.error(reason)
+  })
+
+  process.on('unhandledRejection', (reason) => {
+    logger.log('unhandled rejection')
+    logger.error(reason)
+  })
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
@@ -24,5 +39,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(port);
+
+  logger.log(`app starteds on port ${port}`)
 }
 bootstrap();
